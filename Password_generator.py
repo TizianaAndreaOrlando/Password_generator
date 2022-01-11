78c15fda7df8d539d1dc9b1d1e8a18b584ff3f81
TizianaAndreaOrlando <81337862+TizianaAndreaOrlando@users.noreply.github.com>
2022-01-11 19:56:11 -0300

    Add files via upload

---
+++ Password_generator.py
@@ -0,0 +1,26 @@
+import random 
+from lists import *
+#********************   **************   **************
+#From here we will input our data
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+#********************   **************   **************
+#Building our password with for loop
+
+password = []
+for number in range(nr_letters):
+  password.append(random.choice(letters))
+for number in range(nr_symbols):
+  password.append(random.choice(symbols))
+for number in range(nr_numbers):
+  password.append(random.choice(symbols))
+
+
+# We will change the password order to make it safer.
+
+password = random.sample(password,len(password))
+password = ''.join(password) 
+print(f"\nYour password is: {password}")
